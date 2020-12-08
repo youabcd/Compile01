981d015b399a3f581721e1504e8eccc570fbd662
youabcd <63175220+youabcd@users.noreply.github.com>
2020-12-08 19:53:45 +0800

    Update App.java

--- src/main/java/miniplc0java/App.java
+++ src/main/java/miniplc0java/App.java
@@ -44,7 +44,7 @@ public class App {
         }
 
         var inputFileName = result.getString("input");
-        var outputFileName = result.getString("output");
+        var outputFileName = result.getString("asm");
 
         InputStream input;
         if (inputFileName.equals("-")) {
