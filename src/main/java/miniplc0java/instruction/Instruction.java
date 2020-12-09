25caf3a840c0b53a317cdfa29708be81873c52c5
youabcd <63175220+youabcd@users.noreply.github.com>
2020-12-09 20:00:48 +0800

    命令修改

--- src/main/java/miniplc0java/instruction/Instruction.java
+++ src/main/java/miniplc0java/instruction/Instruction.java
@@ -28,36 +28,36 @@ public class Instruction {
 
     public static HashMap<String, Number> INSTRUCT = new HashMap<>();
     static{
-        INSTRUCT.put("nop", 0x00);INSTRUCT.put("push", 0x01);
-        INSTRUCT.put("pop", 0x02);INSTRUCT.put("popn", 0x03);
-        INSTRUCT.put("dup", 0x04);INSTRUCT.put("loca", 0x0a);
-        INSTRUCT.put("arga", 0x0b);INSTRUCT.put("globa", 0x0c);
-        INSTRUCT.put("load_8", 0x10);INSTRUCT.put("load_16", 0x11);
-        INSTRUCT.put("load_32", 0x12);INSTRUCT.put("load_64", 0x13);
-        INSTRUCT.put("store_8", 0x14);INSTRUCT.put("store_16", 0x15);
-        INSTRUCT.put("store_32", 0x16);INSTRUCT.put("store_64", 0x17);
-        INSTRUCT.put("alloc", 0x18);INSTRUCT.put("free", 0x19);
-        INSTRUCT.put("stackalloc", 0x1a);INSTRUCT.put("add_i", 0x20);
-        INSTRUCT.put("sub_i", 0x21);INSTRUCT.put("mul_i", 0x22);
-        INSTRUCT.put("div_i", 0x23);INSTRUCT.put("add_f", 0x24);
-        INSTRUCT.put("sub_f", 0x25);INSTRUCT.put("mul_f", 0x26);
-        INSTRUCT.put("div_f", 0x27);INSTRUCT.put("div_u", 0x28);
-        INSTRUCT.put("shl", 0x29);INSTRUCT.put("shr", 0x2a);
-        INSTRUCT.put("and", 0x2b);INSTRUCT.put("or", 0x2c);
-        INSTRUCT.put("xor", 0x2d);INSTRUCT.put("not", 0x2e);
-        INSTRUCT.put("cmp_i", 0x30);
-        INSTRUCT.put("cmp_f", 0x32);INSTRUCT.put("cmp_u", 0x31);
-        INSTRUCT.put("neg_i", 0x34);INSTRUCT.put("neg_f", 0x35);
-        INSTRUCT.put("itof", 0x36);INSTRUCT.put("ftoi", 0x37);
-        INSTRUCT.put("shrl", 0x38);INSTRUCT.put("set_lt", 0x39);
-        INSTRUCT.put("set_gt", 0x3a);INSTRUCT.put("br", 0x41);
-        INSTRUCT.put("br_false", 0x42);INSTRUCT.put("br_true", 0x43);
-        INSTRUCT.put("call", 0x48);INSTRUCT.put("ret", 0x49);
-        INSTRUCT.put("callname", 0x4a);INSTRUCT.put("scan_i", 0x50);
-        INSTRUCT.put("scan_c", 0x51);INSTRUCT.put("scan_f", 0x52);
-        INSTRUCT.put("print_i", 0x54);INSTRUCT.put("print_c", 0x55);
-        INSTRUCT.put("print_f", 0x56);INSTRUCT.put("print_s", 0x57);
-        INSTRUCT.put("println", 0x58);INSTRUCT.put("panic", 0xfe);
+        INSTRUCT.put("Nop", 0x00);INSTRUCT.put("Push", 0x01);
+        INSTRUCT.put("Pop", 0x02);INSTRUCT.put("Popn", 0x03);
+        INSTRUCT.put("Dup", 0x04);INSTRUCT.put("LocA", 0x0a);
+        INSTRUCT.put("ArgA", 0x0b);INSTRUCT.put("GlobA", 0x0c);
+        INSTRUCT.put("Load8", 0x10);INSTRUCT.put("Load16", 0x11);
+        INSTRUCT.put("Load32", 0x12);INSTRUCT.put("Load64", 0x13);
+        INSTRUCT.put("Store8", 0x14);INSTRUCT.put("Store16", 0x15);
+        INSTRUCT.put("Store32", 0x16);INSTRUCT.put("Store64", 0x17);
+        INSTRUCT.put("Alloc", 0x18);INSTRUCT.put("Free", 0x19);
+        INSTRUCT.put("StackAlloc", 0x1a);INSTRUCT.put("AddI", 0x20);
+        INSTRUCT.put("SubI", 0x21);INSTRUCT.put("MulI", 0x22);
+        INSTRUCT.put("DivI", 0x23);INSTRUCT.put("AddF", 0x24);
+        INSTRUCT.put("SubF", 0x25);INSTRUCT.put("MulF", 0x26);
+        INSTRUCT.put("DivF", 0x27);INSTRUCT.put("DivU", 0x28);
+        INSTRUCT.put("Shl", 0x29);INSTRUCT.put("Shr", 0x2a);
+        INSTRUCT.put("And", 0x2b);INSTRUCT.put("Or", 0x2c);
+        INSTRUCT.put("Xor", 0x2d);INSTRUCT.put("Not", 0x2e);
+        INSTRUCT.put("CmpI", 0x30);
+        INSTRUCT.put("CmpF", 0x32);INSTRUCT.put("CmpU", 0x31);
+        INSTRUCT.put("NegI", 0x34);INSTRUCT.put("NegF", 0x35);
+        INSTRUCT.put("ItoF", 0x36);INSTRUCT.put("FtoI", 0x37);
+        INSTRUCT.put("Shrl", 0x38);INSTRUCT.put("SetLt", 0x39);
+        INSTRUCT.put("SetGt", 0x3a);INSTRUCT.put("Br", 0x41);
+        INSTRUCT.put("BrFalse", 0x42);INSTRUCT.put("BrTrue", 0x43);
+        INSTRUCT.put("Call", 0x48);INSTRUCT.put("Ret", 0x49);
+        INSTRUCT.put("CallName", 0x4a);INSTRUCT.put("ScanI", 0x50);
+        INSTRUCT.put("ScanC", 0x51);INSTRUCT.put("ScanF", 0x52);
+        INSTRUCT.put("PrintI", 0x54);INSTRUCT.put("PrintC", 0x55);
+        INSTRUCT.put("PrintF", 0x56);INSTRUCT.put("PrintS", 0x57);
+        INSTRUCT.put("PrintLN", 0x58);INSTRUCT.put("Panic", 0xfe);
     }
 
     @Override
@@ -109,68 +109,49 @@ public class Instruction {
 
     public boolean hasX(){
         switch (this.opt) {
-            case Push:
             case Pop:
-            case Br:
+
             case Not:
             case Ret:
             case AddF:
             case AddI:
-            case Call:
             case CmpF:
             case CmpI:
             case DivF:
             case DivI:
             case FtoI:
             case ItoF:
-            case LocA:
             case MulF:
             case MulI:
             case NegF:
             case NegI:
             case SubF:
-
             case SubI:
-
-            case GlobA:
-
+            case Store64:
+            case SetGt:
+            case SetLt:
+            case PrintC:
+            case PrintF:
+            case PrintI:
+            case ScanC:
+            case PrintS:
+            case ScanF:
+            case PrintLN:
+            case ScanI:
             case Panic:
                 return false;
-            case ScanC:
-
-            case ScanF:
-
-            case ScanI:
-
-            case SetGt:
-
-            case SetLt:
-
+            case Call:
+            case Push:
+            case GlobA:
+            case LocA:
             case StackAlloc:
-
             case BrTrue:
-
+            case Br:
             case Load64:
-
-            case PrintC:
-
-            case PrintF:
-
-            case PrintI:
-
-            case PrintS:
-
             case BrFalse:
-
-            case PrintLN:
-
-            case Store64:
-
             case ArgA:
-
             case CallName:
                 return true;
-
             default:
                 return false;
         }
