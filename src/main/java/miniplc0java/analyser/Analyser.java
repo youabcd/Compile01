7603978158763754fc93e336593ae5bc255c5d37
youabcd <63175220+youabcd@users.noreply.github.com>
2020-12-22 10:35:53 +0800

    let voidbug修复

--- src/main/java/miniplc0java/analyser/Analyser.java
+++ src/main/java/miniplc0java/analyser/Analyser.java
@@ -794,6 +794,9 @@ public final class Analyser {
         Token k=expect(TokenType.Ident);
         expect(TokenType.Colon);
         Token t=expect(TokenType.Ty);
+        if(t.getValueString().equals("void")){
+            throw new AnalyzeError(ErrorCode.NotDeclared,k.getStartPos());
+        }
         String type="";
         int offset;
         if(depth==0){
