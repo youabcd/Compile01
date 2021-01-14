c0df20efbd9d44051b28fcd6ac3665c12ad1516d
youabcd <63175220+youabcd@users.noreply.github.com>
2021-01-14 17:44:06 +0800

    0

---
+++ src/main/java/miniplc0java/vm/MiniVm.java
@@ -0,0 +1,97 @@
+package miniplc0java.vm;
+
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import miniplc0java.instruction.Instruction;
+
+public class MiniVm {
+    private List<Instruction> instructions;
+    private PrintStream out;
+
+    /**
+     * @param instructions
+     * @param out
+     */
+    public MiniVm(List<Instruction> instructions, PrintStream out) {
+        this.instructions = instructions;
+        this.out = out;
+    }
+
+    public MiniVm(List<Instruction> instructions) {
+        this.instructions = instructions;
+        this.out = System.out;
+    }
+
+    private ArrayList<Integer> stack = new ArrayList<>();
+
+    private int ip;
+
+    public void Run() {
+        ip = 0;
+        while (ip < instructions.size()) {
+            var inst = instructions.get(ip);
+            RunStep(inst);
+            ip++;
+        }
+    }
+
+    private Integer pop() {
+        var val = this.stack.get(this.stack.size() - 1);
+        this.stack.remove(this.stack.size() - 1);
+        return val;
+    }
+
+    private void push(Integer i) {
+        this.stack.add(i);
+    }
+
+    private void RunStep(Instruction inst) {
+        switch (inst.getOpt()) {
+            case CallName:{}
+            case Store64:{}
+            case PrintLN:{}
+            case BrFalse:{}
+            case PrintS:{}
+            case PrintI:{}
+            case PrintF:{}
+            case PrintC:{}
+            case Load64:{}
+            case BrTrue:{}
+            case SetLt:{}
+            case SetGt:{}
+            case ScanI:{}
+            case ScanF:{}
+            case ScanC:{}
+            case Panic:{}
+            case GlobA:{}
+            case SubI:{}
+            case SubF:{}
+            case NegI:{}
+            case NegF:{}
+            case MulI:{}
+            case MulF:{}
+            case LocA:{}
+            case ItoF:{}
+            case FtoI:{}
+            case DivI:{}
+            case DivF:{}
+            case CmpI:{}
+            case CmpF:{}
+            case Call:{}
+            case AddI:{}
+            case AddF:{}
+            case Ret:{}
+            case Not:{}
+            case Br:{}
+            case Pop:{}
+            case StackAlloc:{}
+            case ArgA:{}
+            case Push:{}
+                break;
+            default:
+                break;
+        }
+    }
+}
